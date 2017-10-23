6f1fec9a8e81f8c3bd2977a8b7fb14897bb25a99
Nikolaos Barkas <barkasn@gmail.com>
2017-10-23 11:13:33 -0400

    conditional omp compile

--- src/hogsvd.cpp
+++ src/hogsvd.cpp
@@ -1,7 +1,10 @@
 #include <RcppArmadillo.h>
 #include <vector>
 
+#ifdef _OPENMP
 #include <omp.h>
+#endif
+
 // [[Rcpp::plugins(openmp)]]
 
 using namespace Rcpp;
@@ -11,31 +14,46 @@ using namespace Rcpp;
 //' Calculate normalised S matrix
 //' @param D list of data matrices
 //' @param ncols number of columns of input matrices
+//' @param nthreads number of omp threads, 0 for max
 //' @export
 // [[Rcpp::export]]
-arma::mat calcNormS(const List& D, int& ncols) {
+arma::mat calcNormS(const List& D, int& ncols, int nthreads = 1) {
   arma::uword N = D.size();
   std::vector<arma::mat> A(N);
   std::vector<arma::mat> Ainv(N);
   arma::mat S(ncols, ncols, arma::fill::zeros);
-
-  #pragma omp parallel for
+  
+#ifdef _OPENMP
+  if (nthreads == 0) {
+    nthreads = omp_get_max_threads(); 
+  }
+  
+  omp_set_num_threads(nthreads);
+#endif
+  
+#pragma omp parallel for
   for ( arma::uword i = 0; i < N; i++) {
     arma::mat Di = as<arma::mat>(D[i]); 
     arma::mat matA = Di.t() * Di;
     A[i] = matA;
     Ainv[i] = arma::inv(matA);
   }
-
-  #pragma omp parallel for
+  
+#pragma omp parallel for
   for ( arma::uword i = 0; i < N; i++ ) {
     for ( arma::uword j = i + 1; j < N; j++) {
       arma::mat tmp = A[i] * Ainv[j] + A[j] * Ainv[i]; 
-      #pragma omp critical
-      S = S + tmp;
+#pragma omp critical
+{
+  S = S + tmp;
+}
     }
   }
-    
+  
+  // Force thread sync
+#pragma omp barrier  
+  
+  
   S = S / (N * (N - 1));
   
   return S;
